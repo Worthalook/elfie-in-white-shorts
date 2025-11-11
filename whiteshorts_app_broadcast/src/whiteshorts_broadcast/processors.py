4836a836c81926aef86d1225ed542290649f1764
gareth-reid <garethreid123@gmail.com>
2025-11-12 02:18:57 +1100

    Update processors.py

--- whiteshorts_app_broadcast/src/whiteshorts_broadcast/processors.py
+++ whiteshorts_app_broadcast/src/whiteshorts_broadcast/processors.py
@@ -74,7 +74,7 @@ def default_pipeline(df: pd.DataFrame, cfg) -> list[dict]:
     df2 = normalize_columns(df2, cfg.rename_map)
     df2 = coerce_types(df2)
     df2 = normalize_dates(df2)
-    df2 = clip_columns(df2)
+    df2 = clip_columns(df2, {"pred_mean": (0.5, 10), "pred_q10": (0, 10), "pred_q90": (0.5, 10)})
     df2 = nullify_non_finite(df2)     # <- critical for JSON
     df2 = drop_missing_required(df2, cfg.required_cols)
     for fn in cfg.processors:
